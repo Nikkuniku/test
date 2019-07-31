ab605e30e02db8e2fd1feab2c4ea30de438d2c41
Nikkuniku <39636921+Nikkuniku@users.noreply.github.com>
2019-07-31 22:05:37 +0900

    テーブルの数値に対してカンマをつけるプラグイン
    実行する際は、チェックボタンを追加したいテーブルのidをtest_tableに変えて
    $(function () {
                $('#test_table>tbody>tr').Pricecomma();
            })
    
    をscriptに書いてあげればよいです
    （もちろんidは好きなものに変えてもOK）

---
+++ Pricecomma.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    $.fn.Pricecomma = function () {
+        this.each(function () {
+            //テーブルのカンマをつけたい行番号
+            var col_num = 4
+            var num = $(this).children('td').eq(col_num - 1).text();
+            num = (num - 0).toLocaleString();
+            $(this).children('td').eq(col_num - 1).text(num)
+        });
+    };
+})(jQuery);
