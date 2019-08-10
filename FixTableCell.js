e83a8ef14f15f0959c50407bcf74eb0950e8172a
Nikkuniku <39636921+Nikkuniku@users.noreply.github.com>
2019-08-10 20:55:02 +0900

    【テーブル】文字列の長さに応じて表示を調整するプラグイン
    $(function () {
                $('table>tbody>tr').FixTable();
                    });
    と書くことで実行できます。

---
+++ FixTableCell.js
@@ -0,0 +1,31 @@
+﻿(function ($) {
+    $.fn.FixTable = function () {
+        this.each(function () {
+            //特定列の文字長を調整して表示する
+            var col_num = 1
+            var book_nm = $(this).children('td').eq(col_num - 1).text();
+            if (getLen(book_nm) > 26) {
+                var cut_book_nm = book_nm.slice(0, 25);
+                cut_book_nm += '...'
+                $(this).children('td').eq(col_num - 1).text(cut_book_nm);
+            }
+            $(this).children('td').eq(col_num - 1).css({
+                'font-size': '90%'
+            });
+
+            //調整したい列番号を格納する。
+            var col_array = [2, 3];
+            for (var i = 0; i < col_array.length; i++) {
+                var book_data = $(this).children('td').eq(col_array[i] - 1).text();
+                if (getLen(book_nm) > 11) {
+                    var cut_book_data = book_data.slice(0, 11);
+                    cut_book_data += '...'
+                    $(this).children('td').eq(col_array[i] - 1).text(cut_book_data);
+                }
+                $(this).children('td').eq(col_array[i] - 1).css({
+                    'font-size': '90%'
+                });
+            };
+        });
+    };
+})(jQuery);
